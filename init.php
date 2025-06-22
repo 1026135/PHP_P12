607a3da1c13247e22b63a146aed90d45a48698de
TimBlokdijk <1026135@mborijnland.nl>
2025-06-22 18:02:08 +0200

    Update init.php

--- init.php
+++ init.php
@@ -3,13 +3,14 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once __DIR__ . '/config/config.php';
-require_once __DIR__ . '/helpers/functions.php';
+define('ROOT_PATH', __DIR__ . '/'); //root path to C:\xampp\htdocs\Github code\PHP_P12\
+
+require_once ROOT_PATH . 'config/config.php';
+require_once ROOT_PATH . 'helpers/functions.php';
 
 spl_autoload_register(function ($class) {
-    $path = __DIR__ . '/classes/' . $class . '.php';
+    $path = ROOT_PATH . 'classes/' . $class . '.php';
     if (file_exists($path)) {
         require_once $path;
     }
 });
-?>
