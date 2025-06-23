2f1ae28bbfcfe3e98287286793b848dad2fd7eca
TimBlokdijk <1026135@mborijnland.nl>
2025-06-23 10:55:41 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -5,6 +5,8 @@ define("DB_NAME", "user_crud" );
 define("DB_USER", "root" );
 define("DB_PASS", "" );
 
-define('BASE_URL', '/Github_code/PHP_P12/');  // URL root path [http://http://localhost/Github_code/PHP_P12/]
+//define('BASE_URL', '/Github_code/PHP_P12/');  // URL root path [http://http://localhost/Github_code/PHP_P12/]
 // define('BASE_URL', '/'); // URL root path [http://localhost/]
+
+define('BASE_URL', '/Github_PHP/PHP_P12/');  // URL root path [http://http://localhost/Github_PHP/PHP_P12/]
 ?>
